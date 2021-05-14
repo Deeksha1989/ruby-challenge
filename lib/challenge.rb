76c0b2657f4eaacbac60a0e4578e106f08456648
Harshitavash5 <84200429+Deeksha1989@users.noreply.github.com>
2021-05-14 11:37:00 -0700

    Ruby challenges

--- lib/challenge.rb
+++ lib/challenge.rb
@@ -1,19 +1,33 @@
-require "time"
-
+require 'time'
+# challenge solution to find latest and earliest date from file
 class Challenge
-  attr_reader :earliest_time
-  attr_reader :latest_time
-  attr_reader :peak_year
+  attr_reader :earliest_time, :latest_time, :peak_year
 
   def initialize(file_path)
     @file_path = File.expand_path(file_path)
   end
 
   def parse
-    # Parse the file located at @file_path and set three attributes:
-    #
-    # earliest_time: the earliest time contained within the data set
-    # latest_time: the latest time contained within the data set
-    # peak_year: the year with the most number of timestamps contained within the data set
+    @hash_of_years = {}
+    File.open(@file_path, 'r') do |f|
+      @earliest_time = Time.parse(f.first)
+      @latest_time = @earliest_time
+      f.each_line do |line|
+        find_earliest_latest_date_and_year(line)
+      end
+    end
+    @peak_year = @hash_of_years.key(@hash_of_years.values.max).to_s.to_i
+  end
+
+  def find_earliest_latest_date_and_year(line)
+    parsed_time = Time.parse(line)
+    @earliest_time = @earliest_time < parsed_time ? @earliest_time : parsed_time
+    @latest_time = @latest_time > parsed_time ? @latest_time : parsed_time
+    year = parsed_time.year
+    if @hash_of_years.key?(:"#{year}")
+      @hash_of_years[:"#{year}"] += 1
+    else
+      @hash_of_years.merge!("#{year}": 1)
+    end
   end
 end
